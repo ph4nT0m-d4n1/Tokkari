badf5eab821ad582c49e0ea596f55b65d49e015f
ph4nT0m-d4n1 <dmallya628@gmail.com>
2024-11-21 11:51:23 -0500

    added sound test script

---
+++ Tokkari_Unity/Assets/Tokkari/Code/Sound.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public float sensitivity = 1;
+    public GameObject seal;
+    public float rms;
+
+    AudioSource audioSource;
+    Transform t;
+    Vector3 s;
+    float sy;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(Microphone.devices[0], true, 10, 44100);
+        audioSource.loop = true;
+        //audioSource.volume = .01f;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play();
+        t = seal.transform;
+        s = t.localPosition;
+        sy = s.y;
+    }
+
+    private void Update()
+    {
+        s.y = sy * rms * sensitivity;
+        t.localPosition = s;
+    }
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        rms = 0;
+
+        for (int i = 0; i < data.Length; ++i)
+        {
+            rms += data[i] * data[i];
+        }
+
+        rms = Mathf.Sqrt(rms / data.Length);
+    }
+}
+
