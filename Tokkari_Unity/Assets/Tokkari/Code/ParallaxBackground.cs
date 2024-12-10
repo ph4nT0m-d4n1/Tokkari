443973a9c5573f8d1effaa798946af2197b31d0e
ph4nT0m-d4n1 <dmallya628@gmail.com>
2024-12-10 16:25:10 -0500

    final implementations (part 3)
    - added comments to the parallax script

--- Tokkari_Unity/Assets/Tokkari/Code/ParallaxBackground.cs
+++ Tokkari_Unity/Assets/Tokkari/Code/ParallaxBackground.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class ParallexBackground : MonoBehaviour
 {
-    public float moveSpeed;
-    public bool scrollLeft;
-    private float textureWidth;
+    public float moveSpeed; //how fast the background item moves
+    public bool scrollLeft; //whether or not the item moves left
+    private float textureWidth; //the sprite width, for repeating purposes
 
     void Start()
     {
@@ -14,20 +14,21 @@ public class ParallexBackground : MonoBehaviour
 
         if(scrollLeft == true)
         {
-            moveSpeed = -moveSpeed;
+            moveSpeed = -moveSpeed; //changing the movement direction
         }
     }
 
     void Scroll()
     {
-        float del = moveSpeed * Time.deltaTime;
+        float del = moveSpeed * Time.deltaTime; //delta = movement speed * time, same thing here
         transform.position += new Vector3 (del, 0f, 0f);
     }
 
     void Reset()
     {
-        if ((Math.Abs(transform.position.x) - textureWidth) > 0)
+        if ((Math.Abs(transform.position.x) - textureWidth) > 0) //finding the absolute value of the x position in relation to the texture's width
         {
+            //resetting the x position when the object is out of camera's view (i.e. when the texture's width has passed the camera)
             transform.position = new Vector3(0.0f, transform.position.y, transform.position.z);
         }
     }
